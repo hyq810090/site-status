75107beab2484d3e75c39f0c59108bb2b6733b96
hyq810090 <131674904+hyq810090@users.noreply.github.com>
2024-10-01 00:55:32 +0800

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
         <p>
           <CustomLink
             text={Package.alia}
-            to="https://github.com/imsyy/site-status"
+            to="https://github.com/hyq810090/"
           />
           &nbsp;Version&nbsp;{Package.version}
         </p>
@@ -34,9 +34,9 @@ const Footer = () => {
           &nbsp;接口&nbsp;|&nbsp;检测频率 5 分钟
         </p>
         <p>
-          Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
+          &nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
           &nbsp;
-          <CustomLink to="https://www.imsyy.top/" text="無名" />
+          <CustomLink to="https://home.hyqai.top/" text="HYQ" />
           {siteIcp ? (
             <React.Fragment>
               &nbsp;|&nbsp;
